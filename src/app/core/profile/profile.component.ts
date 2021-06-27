091a866fbfc92f41021a9d166139e1dfd851a182
Luis Fernando Hernandez <hluisfernando1@gmail.com>
2021-06-27 12:33:41 -0500

    organizing the profile screen

--- src/app/core/profile/profile.component.ts
+++ src/app/core/profile/profile.component.ts
@@ -26,15 +26,5 @@ export class ProfileComponent implements OnInit {
   }
 
 
-  onSelectFile(event) {
-    if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
-      reader.readAsDataURL(event.target.files[0]); // read file as data url
-      reader.onload = (event) => { // called once readAsDataURL is completed
-        this.url = event.target.result;
-      }
-    }
-  }
-
 
 }
